321ffdcf7aa6dbefcf2b1f2d6b0b02689513bda9
Tacs25 <90818953+Tacs25@users.noreply.github.com>
2022-02-24 21:32:59 +0800

    Update update.php

--- main/patient/update.php
+++ main/patient/update.php
@@ -16,20 +16,51 @@ session_start();
 				<div class="card-body">
 					<form method="post" id="patient_register_form" action="../includes/update.inc.php">
 						<div class="form-group">
+							
 							<label>Current Password<span class="text-danger"></label>
+							<div class="input-group">
 							<input type="password" name="current_password" id="current_password" class="form-control" required  />
+							<div class="input-group-prepend">
+								<div class="input-group-text">
+									<a href="#" class="text-dark" id="icon-click">
+										<i class="fas fa-eye" id="icon"></i>
+									</a>
+								</div>
+								</div>
+							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
+
 								<div class="form-group">
+
 									<label>New Password<span class="text-danger"></label>
+									<div class="input-group">
 									<input type="password" name="new_password" id="new_password" class="form-control" required   />
+									<div class="input-group-prepend">
+										<div class="input-group-text">
+											<a href="#" class="text-dark" id="icon-click1">
+												<i class="fas fa-eye" id="icon1"></i>
+											</a>
+										</div>
+									</div>
+								</div>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
+
 									<label>Confirm Password<span class="text-danger"></label>
+									<div class="input-group">
 									<input type="password" name="repeat_password" id="repeat_password" class="form-control" required   />
+									<div class="input-group-prepend">
+										<div class="input-group-text">
+											<a href="#" class="text-dark" id="icon-click2">
+												<i class="fas fa-eye" id="icon2"></i>
+											</a>
+										</div>
+									</div>
+								</div>
 								</div>
 							</div>
 						</div>
@@ -68,4 +99,55 @@ session_start();
           				<?php
           			}
       			}
+			?>
 
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		
+		$("#icon-click").click(function(){
+			$("#icon").toggleClass('fa-eye-slash');
+
+			var input = $("#current_password");
+			if(input.attr("type") === "password"){
+				input.attr("type","text");
+			}
+			else
+			{
+				input.attr("type","password");
+			}
+			
+		});
+		$("#icon-click1").click(function(){
+			$("#icon1").toggleClass('fa-eye-slash');
+
+			var input = $("#new_passwordrp");
+			if(input.attr("type") === "password"){
+				input.attr("type","text");
+			}
+			else
+			{
+				input.attr("type","password");
+			}
+			
+			
+		});
+		$("#icon-click2").click(function(){
+			$("#icon2").toggleClass('fa-eye-slash');
+
+			var input = $("#repeat_passwordrp");
+			if(input.attr("type") === "password"){
+				input.attr("type","text");
+			}
+			else
+			{
+				input.attr("type","password");
+			}
+			
+			
+		});
+	});
+
+</script>

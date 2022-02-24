c97f4b2cb0bd9b8781427402a8693b1e992d14d6
Tacs25 <90818953+Tacs25@users.noreply.github.com>
2022-02-24 23:15:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,23 +30,23 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
+              <li class="nav-item active">
                 <a class="nav-link" aria-current="page" href="#home">Home</a>
               </li>
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="#about">About</a>
               </li>
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="#services">Services</a>
               </li>
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="#contact">Contact</a>
               </li>
               <?php
               session_start();
               if(isset($_SESSION['id'])){
               ?>
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="main/patient/profile.php">Profile</a>
               </li>
               <?php

9054abe3e2381d5e81adae8468c57b2034247a97
Tacs25 <90818953+Tacs25@users.noreply.github.com>
2022-02-24 21:46:33 +0800

    Update changeforgot.php

--- changeforgot.php
+++ changeforgot.php
@@ -20,14 +20,34 @@ if(isset($_GET['error'])){
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
+
 									<label>New Password<span class="text-danger"></label>
+									<div class="input-group">
 									<input type="password" name="new_password" id="new_password" class="form-control" required   />
+									<div class="input-group-prepend">
+										<div class="input-group-text">
+											<a href="#" class="text-dark" id="icon-click">
+											<i class="fas fa-eye" id="icon"></i>
+										</a>
+									</div>
+								</div>
+							</div>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
+
 									<label>Confirm Password<span class="text-danger"></label>
+									<div class="input-group">
 									<input type="password" name="repeat_password" id="repeat_password" class="form-control" required   />
+									<div class="input-group-prepend">
+										<div class="input-group-text">
+											<a href="#" class="text-dark" id="icon-click1">
+												<i class="fas fa-eye" id="icon1"></i>
+											</a>
+										</div>
+									</div>
+								</div>
 								</div>
 							</div>
 						</div>
@@ -67,3 +87,37 @@ else{
     echo "Something Went Wrong";
 }
 ?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		
+		$("#icon-click").click(function(){
+			$("#icon").toggleClass('fa-eye-slash');
+
+			var input = $("#new_password");
+			if(input.attr("type") === "password"){
+				input.attr("type","text");
+			}
+			else
+			{
+				input.attr("type","password");
+			}
+			
+		});
+		$("#icon-click1").click(function(){
+			$("#icon1").toggleClass('fa-eye-slash');
+
+			var input = $("#repeat_password");
+			if(input.attr("type") === "password"){
+				input.attr("type","text");
+			}
+			else
+			{
+				input.attr("type","password");
+			}			
+		});
+	});
+
+</script>

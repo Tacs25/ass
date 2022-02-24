9b8e75c33b42a1f6afb0161863b2f147021f477e
Tacs25 <90818953+Tacs25@users.noreply.github.com>
2022-02-24 23:12:40 +0800

    Update active.js

--- js/active.js
+++ js/active.js
@@ -2,30 +2,49 @@
 const sections = document.querySelectorAll("section[id]");
 
 // Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
+window.addEventListener("scroll", () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
 
-function navHighlighter() {
+        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute("id");
 
-    // Get current scroll position
-    let scrollY = window.pageYOffset;
-
-    // Now we loop through sections to get height, top and ID values for each
-    sections.forEach(current => {
-        const sectionHeight = current.clientHeight;
-        const sectionTop = current.offsetTop;
-        sectionId = current.getAttribute("id");
-
-        /*
-        - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-        - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-        */
-        if (
-            scrollY >= sectionTop &&
-            scrollY <= sectionTop + sectionHeight
-        ) {
-            document.querySelector(".navbar a[href*=" + sectionId + "]").classList.add("active");
-        } else {
-            document.querySelector(".navbar a[href*=" + sectionId + "]").classList.remove("active");
         }
-    });
-}
+    })
+    navLi.forEach(li => {
+        li.classlist.remove('active');
+        if (li.classlist.contains(current)) {
+            li.classlist.add('active')
+
+        }
+    })
+})
+
+
+// function navHighlighter() {
+
+//     // Get current scroll position
+//     let scrollY = window.pageYOffset;
+
+//     // Now we loop through sections to get height, top and ID values for each
+//     sections.forEach(current => {
+//         const sectionHeight = current.clientHeight;
+//         const sectionTop = current.offsetTop;
+//         sectionId = current.getAttribute("id");
+
+//         /*
+//         - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+//         - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+//         */
+//         if (
+//             scrollY >= sectionTop &&
+//             scrollY <= sectionTop + sectionHeight
+//         ) {
+//             document.querySelector(".navbar a[href*=" + sectionId + "]").classList.add("active");
+//         } else {
+//             document.querySelector(".navbar a[href*=" + sectionId + "]").classList.remove("active");
+//         }
+//     });
+// }

000860d08acd9299649b58ad0ceeb67b39f56a14
Tacs25 <adriantacdol2@gmail.com>
2022-03-27 22:03:07 +0800

    Update dbh.inc.php

--- main/includes/dbh.inc.php
+++ main/includes/dbh.inc.php
@@ -1,16 +1,16 @@
 <?php
 
 // Local Development
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'access2';
-
-// Hosting
 // $servername = 'localhost';
 // $username = 'root';
 // $password = '';
-// $dbname = 'access';
+// $dbname = 'access2';
+
+// Hosting
+$servername = 'remotemysql.com';
+$username = '6VJZxCdZXy';
+$password = 'm4efKGyzN6';
+$dbname = '6VJZxCdZXy';
 
 $conn = mysqli_connect($servername, $username, $password , $dbname);
 
